343d4951eb585f95bbd831f1dd6fbd76d1870864
JessicaDMunozO <jessitae@gmail.com>
2024-01-29 18:34:25 -0500

    proyecto integrador

---
+++ src/main/java/com/health/healthController/HealthControllerApplication.java
@@ -0,0 +1,13 @@
+package com.health.healthController;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthControllerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/health/healthController/HealthControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.health.healthController;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
